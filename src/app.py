05f28e2185c5c438c236f5928bc6cf12a357dc8f
Emanuel Balcazar <emanuelbalcazar13@gmail.com>
2019-02-20 17:14:21 -0300

    Listado de nodos y relaciones

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 # coding=utf-8
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
 from pprint import pprint
 import pyorient
+import json
 
 # sitepackages/pyorient/constants.py
 # cambiar de 36 a 37
@@ -11,7 +12,7 @@ import pyorient
 app = Flask(__name__)
 
 # solo para pruebas
-DATABASE = "meetup"
+DATABASE = "demodb"
 HOST = "localhost"
 PORT = 2424
 USER = "root"
@@ -21,12 +22,45 @@ PASSWORD = "root"
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/api/info', methods=['GET'])
 def info():
     client = pyorient.OrientDB(HOST, PORT)
     session_id = client.connect(USER, PASSWORD)
     client.db_open(DATABASE, USER, PASSWORD)
+
     records = client.query("SELECT name FROM OUser where name = 'admin'")
     client.db_close()
 
     return jsonify(records[0].oRecordData)
+
+
+@app.route('/api/nodes', methods=['GET'])
+def get_nodes():
+    client = pyorient.OrientDB(HOST, PORT)
+    session_id = client.connect(USER, PASSWORD)
+    client.db_open(DATABASE, USER, PASSWORD)
+
+    records = client.query("SELECT FROM V")
+    result = [];
+
+    for node in records:
+        result.append({"id": node._rid, "label": node.oRecordData['Name']})
+
+    client.db_close()
+    return jsonify(result)
+
+
+@app.route('/api/edges', methods=['GET'])
+def get_edges():
+    client = pyorient.OrientDB(HOST, PORT)
+    session_id = client.connect(USER, PASSWORD)
+    client.db_open(DATABASE, USER, PASSWORD)
+    records = client.query("SELECT FROM E")
+    result = [];
+
+    for edge in records:
+        result.append({"id": edge._rid , "label": edge._class, "from": str(edge._out), "to": str(edge._in)})
+
+    client.db_close()
+    return jsonify(result)
