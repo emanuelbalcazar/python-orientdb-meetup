4bc2e4b3a6c27f0d3198175f171874a9ff7ec358
Emanuel Balcazar <emanuelbalcazar13@gmail.com>
2019-02-22 16:15:39 -0300

    Demo test corregido

--- src/test/test_database.py
+++ src/test/test_database.py
@@ -1,21 +1,17 @@
 # coding=utf-8
-# para que funcione cambiar
 from pprint import pprint
-
-# from objbrowser import browse
 
 # sitepackages/pyorient/constants.py
 # cambiar de 36 a 37
 # SUPPORTED_PROTOCOL = 37
 
 import pyorient
-print ('empezamos!')
 
 client = pyorient.OrientDB("localhost", 2424)
 session_id = client.connect( "root", "root")
 client.db_open("demodb", "root", "root")
 records = client.query('select from OUser')
 
-print (records)
+print (records[0].oRecordData)
 
 client.db_close()
